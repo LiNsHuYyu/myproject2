20e99a1661ef27bd0abc61cc84f14d9c49d59f1f
MichelleLin <mich030403@gmail.com>
2022-03-04 15:25:43 +0800

    ScoreNG

---
+++ src/main/java/fju/edu/ScoreNG.java
@@ -0,0 +1,14 @@
+package fju.edu;
+
+public class ScoreNG {
+    public static void main(String[] args) {
+        String[] name = {"Bruce","Cheryl","Benny","Chris","Elva"};
+        int[] English = {90,66,78,95,61};
+        int[] Math = {88,46,77,20,58};
+        for (int i = 0; i < 5; i++) {
+            if(i!=4){
+                System.out.println(name[i] + "\t" + English[i] + "\t" + Math[i] + "\t" + (English[i]+Math[i])/2);
+            }
+        }
+    }
+}
