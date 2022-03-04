387da38e96dae20067d5ee9a4773d5db28f1183c
MichelleLin <mich030403@gmail.com>
2022-03-04 16:09:32 +0800

    Score better

---
+++ src/main/java/fju/edu/Scoring.java
@@ -0,0 +1,15 @@
+package fju.edu;
+
+public class Scoring {
+    public static void main(String[] args) {
+        Student bruce = new Student();
+        bruce.name = "Bruce";
+        bruce.english = 90;
+        bruce.math = 88;
+        Student cheryl = new Student();
+        cheryl.name = "Cheryl";
+        cheryl.english = 66;
+        cheryl.math = 46;
+        System.out.println(bruce.name);
+    }
+}
---
+++ src/main/java/fju/edu/Student.java
@@ -0,0 +1,7 @@
+package fju.edu;
+
+public class Student {
+    String name;
+    int english;
+    int math;
+}
