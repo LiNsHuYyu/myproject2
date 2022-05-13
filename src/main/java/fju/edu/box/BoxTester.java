86de51d4f524185f157dd199e8ecf1f76eeb0670
LiNsHuYyu <mich030403@gmail.com>
2022-05-13 16:08:52 +0800

    Parking Selftry

--- src/main/java/fju/edu/box/BoxTester.java
+++ src/main/java/fju/edu/box/BoxTester.java
@@ -1,27 +1,5 @@
 package fju.edu.box;
 
-/*
-    郵局有三種郵便箱，名稱為 一號箱、二號箱與三號箱，其大小分別為(長寬高)：
-    一號箱：10/10/10
-    二號箱：20/20/20
-    三號箱：30/30/30
-    以下程式為一個讓使用者輸入欲寄送物品的長寬高，幫他選擇合適的郵便箱程式。
-    請在「不修改 BoxTester 與 Box 類別」的前提下，完成必要的類別設計，使其能正常執行如下：
-    Please enter object's length: 8
-    Please enter object's width: 8
-    Please enter object's height: 8
-    一號箱
-
-    Please enter object's length: 12
-    Please enter object's width: 8
-    Please enter object's height: 10
-    二號箱
-
-    Please enter object's length: 13
-    Please enter object's width: 10
-    Please enter object's height: 21
-    三號箱
- */
 
 import java.util.ArrayList;
 import java.util.List;
---
+++ src/main/java/fju/edu/parking/Runner.java
@@ -0,0 +1,44 @@
+package fju.edu.parking;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Runner {
+    public static void main(String[] args) {
+        String id = "ABC-1234";
+        String enterTime = "09:00";
+        String exitTime = "12:15";
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+        Date d = null;
+        try{
+            d = sdf.parse(enterTime);
+        }catch (Exception e){
+            System.out.println("Wrong Format");
+        }
+        System.out.println(d);
+        System.out.println(d.getTime());
+        //long ms = 3*60*60*1000;
+        Date d2 = null;
+        try{
+            d2 = sdf.parse(exitTime);
+        }catch (Exception e){
+            System.out.println("Wrong Format");
+        }
+        //d2.setTime(d.getTime()+ms);
+        System.out.println(d2);
+        System.out.println(d2.getTime());
+        int mins = (int)(d2.getTime()-d.getTime())/1000/60;
+        System.out.println(mins);
+        int fees = 30*(mins/60);
+        /*if(mins%60>=15){
+            fees+=30;
+        }*/
+        if(mins%60<=30){
+            fees+=15;
+        }
+        System.out.println("Fee:"+fees);
+        //one hour(60minutes) NT 30
+        //3:14 => NT 90 , 3:15 => NT 120
+        //3:15~3:30 => NT 105
+    }
+}
